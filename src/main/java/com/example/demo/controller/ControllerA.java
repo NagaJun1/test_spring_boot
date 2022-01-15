da23474df8dfeac3768c9d9d7aabe7b4252f0337
NagaJun1 <jumpei603@gmail.com>
2022-01-16 01:02:24 +0900

    メソッド修正

--- src/main/java/com/example/demo/controller/ControllerA.java
+++ src/main/java/com/example/demo/controller/ControllerA.java
@@ -1,27 +1,40 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * http://localhost:8080/ で動く
- * @author user1
+ * http://localhost:8080/controller で呼び出せる
  */
 @RestController
+@RequestMapping("/controller")
 public class ControllerA {
-	@GetMapping
-	public int getA() {
-		return methodB();
+
+	/**
+	 * GETメソッド
+	 * http://localhost:8080/controller/get_method で呼び出せる
+	 * 
+	 * @param param1 URL リクエストで ?get_param=~ で送信できるパラメータ
+	 */
+	@GetMapping("/get_method")
+	public String getA(@RequestParam("get_param") String param1) {
+		// テスト用のURL
+		// http://localhost:8080/controller/get_method?get_param=test
+		return "get メソッドのレスポンス_param->" + param1;
 	}
 
-
-	private static int methodB() {
-		ClassA a = new ClassA();
-		a.i += 1;
-		return a.i;
-	}
-	
-	private static class ClassA{
-		public int i = 5;
+	/**
+	 * POSTメソッド
+	 * 
+	 * @param param1　URL リクエストで ?post_param=~ で送信できるパラメータ
+	 */
+	@PostMapping("/post_method")
+	public String postMethod(@RequestParam("post_param") String param1) {
+		// テスト用のURL
+		// http://localhost:8080/controller/post_method?post_param=test
+		return "POST　メソッドのレスポンス_param->" + param1;
 	}
 }
